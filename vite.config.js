5d5bdbd9c056675be434dabeac730ed65103b0ee
Mudassir Ahmad <ma6386731@gmail.com>
2026-03-05 16:14:43 +0500

    fix: pdfjs-dist vite config fix

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,20 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  optimizeDeps: {
+    include: ['pdfjs-dist']
+  },
   build: {
     outDir: 'dist',
-    chunkSizeWarningLimit: 1600,
-  },
-  esbuild: {
-    logOverride: { 'this-is-undefined-in-esm': 'silent' }
+    chunkSizeWarningLimit: 2000,
+    rollupOptions: {
+      onwarn(warning, warn) {
+        if (warning.code === 'MODULE_LEVEL_DIRECTIVE') return
+        if (warning.code === 'CIRCULAR_DEPENDENCY') return
+        if (warning.message.includes('Use of eval')) return
+        if (warning.message.includes('externalize')) return
+        warn(warning)
+      }
+    }
   }
 })
